f8662489593b482cd7923c0a726149c8dc401e56
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-02-14 20:14:37 +0400

    Style: finished out on the layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
 	content: ["index.html"],
 	theme: {
+		colors: {
+			// Primary
+
+			main: "hsl(75, 94%, 57%)",
+
+			// Neutral
+
+			white: "hsl(0, 0%, 100%)",
+			grey: "hsl(0, 0%, 20%)",
+			darkGrey: "hsl(0, 0%, 12%)",
+			black: "hsl(0, 0%, 8%)",
+		},
+		fontFamily: {
+			inter: "'Inter', sans-serif",
+		},
 		extend: {},
 	},
 	plugins: [],
